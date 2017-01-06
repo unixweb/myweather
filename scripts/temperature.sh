04c312d549ef9b01e1ee7c650c025624d3b3a44d
Joachim Hummel <joachim.hummel@gmail.com>
2017-01-06 15:42:05 +0100

    Added Scripts for MQTT

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -1,3 +1,3 @@
-TEMP=`python /home/pi/myweather/tmperature.py |head -1`
+TEMP=`python /home/pi/myweather/temperature.py |head -1`
 
 mosquitto_pub -h mqtt.unixweb.de -p 1883  -t abcde/temp1 -m $TEMP
